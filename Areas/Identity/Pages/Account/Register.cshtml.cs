1ed7f4230677358b795c6d2ef5dcb2cbd81730ed
invoteco <invoteco@outlook.com>
2019-12-24 23:43:03 +0300

    Add smtp

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Invoteco.Smtp;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -88,8 +89,39 @@ namespace iBlog.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = user.Id, code = code },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    #region Реализация собственного smtp-провайдера
+                    SmtpProvider smtpp = new SmtpProvider();
+                    smtpp.IsBodyHtml = true;
+                    smtpp.IsSSL = true;
+
+                    smtpp.MessageBody = "<p>Вы получили это письмо потому что кто-то зарегистрировался на сайте invoteco.com и при регистрации указал этот адрес электронной почты.</p>" +
+                        "<p>Если это были Вы, то подтвердите свой e-mail, нажав на ссылку 'Подтвердить'. В противном случае не предпринимайте никаких действий.</p>" +
+                        "<p>После подтверждения e-mail Вы будете перенаправлены на сайт и после входа сможете воспользоваться всей его функциональностью.</p>" + "<a href=" + callbackUrl + ">ПОДТВЕРДИТЬ</a>";
+                    smtpp.MessageSubject = "Подтверждение E-mail";
+                    smtpp.SenderEmail = "confirm@domain.tld";
+                    smtpp.SenderName = "Invoteco";
+                    smtpp.SmtpDomain = "smtp.hosterdomain.tld";
+                    smtpp.SmtpPort = 2525;
+                    smtpp.UserEmail = user.Email;
+
+                    if (smtpp.IsValidPatch())//Если путь к корневой директории соответствует установленному хостинг-провайдером,
+                    {
+                        //то отправляем пользователю письмо со ссылкой для подтверждения e-mail.
+                        await smtpp.AsyncSendMailWithEncodeUrlPassworFree(smtpp.SenderName, smtpp.SenderEmail, smtpp.UserEmail, smtpp.MessageSubject, smtpp.MessageBody, smtpp.IsBodyHtml, smtpp.SmtpDomain, smtpp.SmtpPort, smtpp.IsSSL);
+                    }
+                    else
+                    {
+                        //В противном слуае отправляем письмо о проблеме администратору.
+                        string WarningSubject = "Внимание!";
+                        string AdminEmail = "admin@domain.tld";
+                        string WarningMessage = "Письма для подтверждения e-mail не доставляются. Проверьте настройки.";
+
+                        await smtpp.AsyncSendMailWithEncodeUrlPassworFree(smtpp.SenderName, smtpp.SenderEmail, AdminEmail, WarningSubject, WarningMessage, smtpp.IsBodyHtml, smtpp.SmtpDomain, smtpp.SmtpPort, smtpp.IsSSL);
+                    }
+                    #endregion Реализация собственного smtp-провайдера
+
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
