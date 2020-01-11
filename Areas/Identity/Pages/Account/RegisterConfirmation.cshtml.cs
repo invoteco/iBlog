dc2b70efd9a542f517760ba84dec562665a5ef57
invoteco <invoteco@outlook.com>
2020-01-11 10:38:17 +0300

    before pub

--- Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -34,7 +34,8 @@ namespace iBlog.Areas.Identity.Pages.Account
                 return RedirectToPage("/Index");
             }
 
-            var user = await _userManager.FindByEmailAsync(email);
+            //var user = await _userManager.FindByEmailAsync(email);
+            var user = await _userManager.FindByEmailAsync(email).ConfigureAwait(false);
             if (user == null)
             {
                 return NotFound($"Unable to load user with email '{email}'.");
@@ -46,8 +47,10 @@ namespace iBlog.Areas.Identity.Pages.Account
             DisplayConfirmAccountLink = false;
             if (DisplayConfirmAccountLink)
             {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                //var userId = await _userManager.GetUserIdAsync(user);
+                var userId = await _userManager.GetUserIdAsync(user).ConfigureAwait(false);
+                //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user).ConfigureAwait(false);
                 code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                 EmailConfirmationUrl = Url.Page(
                     "/Account/ConfirmEmail",
